1c47637837f1c6900a4d360c629b3f2be2c17c7f
Tim <timyaohoyt80@gmail.com>
2017-04-09 23:31:35 -0400

    Change Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,27 @@
 import { Component } from '@angular/core';
 
+
+export  class Hero {
+  id:number;
+  name:string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}{{name}}</h1>`,
+  template: `<h1>Hello {{name}}{{name}}</h1>
+              <h1>{{title}}</h1>
+              <h1>{{hero.name}}</h1>
+              <p>{{hero.id}}</p>
+            
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+                              name = 'Angular';
+                              title = 'Tour of Heroes';
+                              // hero = 'windstorm';
+                            hero:Hero = {
+                              id:1,
+                              name: 'windstorm',
+                            }
+
+}
